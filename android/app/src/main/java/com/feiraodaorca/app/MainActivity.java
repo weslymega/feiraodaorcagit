0b1d437926133205dba2e0a8caec5d14cb153cd7
André Rocha Medeiros <andrerochamedeiros80@gmail.com>
2026-05-16 13:22:56 -0300

    fix: googlesocial51

--- android/app/src/main/java/com/feiraodaorca/app/MainActivity.java
+++ android/app/src/main/java/com/feiraodaorca/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.feiraodaorca.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Força a inicialização nativa do Firebase no Android assim que o app abre
+        FirebaseApp.initializeApp(this);
+    }
+}
